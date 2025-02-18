232b619a73dc6cf9af8e9124cb3098598d395d2b
avantikaaaaa <148775214+avantikaaaaa@users.noreply.github.com>
2025-02-18 19:45:37 +0530

    Visualize.py

--- Visualize.py
+++ Visualize.py
@@ -48,17 +48,6 @@ def analyse_visualize_data(df):
     plt.title("Online Order - Book Table", fontsize=16, fontweight='bold')
     plt.show()
 
-    '''Observation: We observe that maximum restaurants provide online ordering but not
-    table booking facility. The number of restaurants providing table booking facility
-    but not online order is the least. More than 15000 restaurants don't provide online ordering
-    as well as table booking facility.
-
-    So in case we are planning to start a restaurant and we want some facility to have a lower priority
-    then we can think about this one'''
-
-    # Observation: 59.3% of the restaurants accept online order and 40.7% don't.
-    # Only 12.8% of the restaurants provide table booking facility whereas 87.2% don't.
-
     label1 = ["Accepted", "Not Accepted"]
     label2 = ["Not Accepted", "Accepted"]
 
@@ -80,11 +69,6 @@ def analyse_visualize_data(df):
     plt.figure(figsize=(10, 8))
     sns.barplot(data=df, x='online_order', y='Cost2plates', hue='book_table', palette='rainbow')
     plt.show()
-
-    '''Observation: Restaurants which don't accept online orders but provide table booking
-    have the highest approx_cost value above Rs 1500, whereas restaurants which accept online orders and
-    provide table booking have an approx_cost near Rs 1000. Restaurants which don't provide table booking
-    have an approx_cost of Rs 400.'''
 
     # Visualizing Online Order vs Rate
 
@@ -143,25 +127,7 @@ def analyse_visualize_data(df):
     sns.lineplot(x='rate', y='votes', data=df)
     plt.title('Rate and Votes', fontsize=20, fontweight='bold')
     plt.show()
-
-    '''Observation:- The line plot indicates that for ratings greater than 4.0 the number of votes
-    is more and for less ratings the number of votes are less. Hence, restaurants with
-    high rating also have high votes.'''
-
-    '''Recommendation for the upcoming restaurants:-
-
-    1) For the up coming resturants i would suggest that they should go for quick bites, snacks sort of theme,
-    as it looks like people are more interested in quick munching rather that lunch or dinner.
-    2) Good Location is also very important for offline customers to have a positive and fresh atmosphere.
-    3) For cusine i would pick either north indian or chinese and one can also make a nice fusion of
-    chinese and north indian dishes. And can be the point of attraction for the customers.
-    4) Good food quality at quite cheap rate is very effective strategy so if possible try to
-    keep the range of cost as low as possible.
-    5) Providing pre-ordering and pre-booking facility for online as well as offline
-    customers for quick delivery of food when customers arrive at restaurant.
-    6) And i would suggest to take a random check on customers to know if they do really enjoy the food,
-    because in the end that is what we want customers satisfaction.'''
-
+   
     # Grouping Types of Restaurents, location wise
 
     df3 = df.groupby(['location', 'Type'])['name'].count()
